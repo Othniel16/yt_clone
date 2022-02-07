a65192f6ed34acaeaadd1470b1f697f685d97483
Othniel Ussher <othnielussher16@gmail.com>
2022-02-07 23:33:50 +0000

    Code progression

--- lib/home.dart
+++ lib/home.dart
@@ -24,12 +24,13 @@ class _HomeState extends State<Home> {
             child: Container(
               color: Colors.white,
               height: Get.height,
-              width: Get.width * 0.07,
+              width: Get.width * 0.09,
               child: Column(
                 children: [
                   const SizedBox(height: 20.0),
                   Container(
                     alignment: Alignment.topCenter,
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
                     height: Get.height * 0.06,
                     child: InkWell(
                       onTap: () {
@@ -47,7 +48,7 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            flex: 18,
+            flex: 17,
             child: Column(
               children: [
                 const SizedBox(height: 10.0),
@@ -60,29 +61,34 @@ class _HomeState extends State<Home> {
                 const SizedBox(height: 8.0),
                 Container(height: 1.0, color: Theme.of(context).dividerColor),
                 Expanded(
-                  child: FutureBuilder<Object>(
-                      future: Methods.getPopularVideos(),
-                      builder: (context, snapshot) {
-                        if (!snapshot.hasData &&
-                            snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                        return GridView.builder(
-                          padding: const EdgeInsets.fromLTRB(20, 20, 40, 40),
-                          shrinkWrap: true,
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 4,
-                            mainAxisSpacing: 40.0,
-                            crossAxisSpacing: 18.0,
-                          ),
-                          itemCount: 24,
-                          itemBuilder: (context, index) => const VideoCard(),
+                  child: FutureBuilder<dynamic>(
+                    future: Methods.getPopularVideos(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData &&
+                          snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
                         );
-                      }),
+                      }
+                      List videos = snapshot.data;
+
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        itemCount: videos.length,
+                        itemBuilder: (context, index) {
+                          Video video = Video.fromMap(videos[index]);
+                          return VideoCard(video: video);
+                        },
+                        padding: const EdgeInsets.fromLTRB(20, 20, 40, 40),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                          mainAxisSpacing: 40.0,
+                          crossAxisSpacing: 18.0,
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ],
             ),
