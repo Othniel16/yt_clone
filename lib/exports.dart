997c2ba40acd10ed96a0983849343f8472ef340a
Othniel Ussher <othnielussher16@gmail.com>
2022-02-07 23:33:30 +0000

    🚢 Update exports

--- lib/exports.dart
+++ lib/exports.dart
@@ -2,12 +2,14 @@
 
 // pub
 export 'package:get/get.dart';
+export 'package:cached_network_image/cached_network_image.dart';
 
 // local
 export 'package:yt_clone/app.dart';
 export 'package:yt_clone/home.dart';
 export 'package:yt_clone/methods.dart';
 export 'package:yt_clone/constants.dart';
+export 'package:yt_clone/models/video.dart';
 export 'package:yt_clone/widgets/header.dart';
 export 'package:yt_clone/widgets/menu_bar.dart';
 export 'package:yt_clone/widgets/side_menu.dart';
